5f241341f6331e0552b91d197b99059b21978ed5
flashxyz <flashxyz@gmail.com>
2016-05-05 23:37:09 +0200

    GREEN: Test for finding a name.

--- src/map.js
+++ src/map.js
@@ -5,7 +5,17 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+
+	var res = [];
+	var i;
+	for(i = 0; i < 3; i++)
+	{
+		if(this._posts[i].indexOf(name) > -1)
+		{
+			res.push(this._posts[i]);
+		}
+	}
+	return res;
+};
 
 module.exports = Map;
