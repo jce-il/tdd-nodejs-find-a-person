eac4f98993512bad27147f2f8f6e0fac8a19ffd0
flashxyz <flashxyz@gmail.com>
2016-05-05 23:38:50 +0200

    GREEN: Test for finding a location.

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@
 function Map(posts) {
 
   this._posts = posts;
-}
+};
 
 Map.prototype.find_a_person = function(name) {
 
@@ -16,6 +16,25 @@ Map.prototype.find_a_person = function(name) {
 		}
 	}
 	return res;
+};
+
+Map.prototype.check_for_location = function(name) {
+
+	var posts = this.find_a_person(name);
+	var indexOfAt = 0;
+
+	for(i = 0; i < posts.length; i++)
+	{
+		indexOfAt = posts[i].indexOf("at");
+		if(indexOfAt > -1)
+		{
+			if(posts[i][indexOfAt + 3] === posts[i][indexOfAt + 3].toUpperCase()){
+
+				return "True";
+			}
+		}
+	}
+	return "False";
 };
 
 module.exports = Map;
