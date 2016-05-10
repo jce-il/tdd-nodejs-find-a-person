69257f3ee1c169e46ac5bcd2cc0e89d21b6115b4
Graos <yerson28890@gmail.com>
2016-05-10 14:55:01 +0300

    RED: test for finding name

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -5,7 +5,17 @@ var Map = require('./../src/map');
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.find_a_person("Or A.")
-    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+    var posts = map.find_a_person("Or A.");
+    expect(posts).to.be.eql(["hi"]);
   });
 });
+
+
+/*describe('Multiply numbers', function() {
+  it('Given a some number, return the multiply to 2', function() {
+    var mul = new Map([6,7,8]);
+    var result = mul.multiply(4);
+    expect(result).to.be.eql(8);
+  });
+});*/
+
