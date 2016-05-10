a9a5f4740f2695667d9449687fbbda5e3ecf0a14
gabrielaes <g.esterkind@gmail.com>
2016-05-10 14:30:36 +0300

    GREEN: Test for finding a name

--- src/map.js
+++ src/map.js
@@ -1,10 +1,12 @@
 
-function Map(posts) {
-  this._posts = posts;
+function Map(posts) 
+{
+	this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return [] ;
-  };
+Map.prototype.find_a_person = function(name) 
+{
+	return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
+};
 
 module.exports = Map;
