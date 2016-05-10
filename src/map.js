148bc3f1d3c20cfa5695f6f7302606a59cf4f81c
David Ohayon <DavidOhayonSe@gmail.com>
2016-05-10 12:25:30 +0300

    find_a_person
    try #1

--- src/map.js
+++ src/map.js
@@ -4,7 +4,19 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
+	var ret_val [this._posts.length]; 
+	var i=0;
+	var j=0;
+	for(i=0;i<this._posts.length;i++){
+		var n = this._posts[i].search(name);
+		if(n!=-1){
+			ret_val[j]=this._posts[i];
+			j++;
+		}
+	}
+	
+	
+  return ret_val;
   };
 
 module.exports = Map;
