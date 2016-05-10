061caad767def758a5f350f72d516335de4711f8
Roi <roizundak@Gmail.com>
2016-05-10 12:41:51 +0300

    GREEN:Find Or

--- src/map.js
+++ src/map.js
@@ -5,7 +5,6 @@ function Map(posts) {
 
 Map.prototype.find_a_person = function(name) {
     return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
-
-  };
+};
 
 module.exports = Map;
