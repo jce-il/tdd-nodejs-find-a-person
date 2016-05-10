35d5103c080eb368e7dd5347d7d04e74a78526ac
gabrielaes <g.esterkind@gmail.com>
2016-05-10 14:37:07 +0300

    GREEN: Test for finding a location

--- src/map.js
+++ src/map.js
@@ -9,4 +9,9 @@ Map.prototype.find_a_person = function(name)
 	return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
 };
 
+Map.prototype.find_a_location = function(name)
+{
+	return true;
+}
+
 module.exports = Map;
