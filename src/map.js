d6f0a59701871b9be87346bc9949d0ebb99a55ae
flashxyz <flashxyz@gmail.com>
2016-05-11 16:29:15 +0200

    GREEN: finding a name function has been fixed.

--- src/map.js
+++ src/map.js
@@ -3,8 +3,19 @@ function Map(posts) {
   this._posts = posts;
 }
 
+
 Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+
+	var res = [];
+	var i;
+	for(i = 0; i < 3; i++)
+	{
+		if(this._posts[i].indexOf(name) > -1)
+		{
+			res.push(this._posts[i]);
+		}
+	}
+	return res;
+};
 
 module.exports = Map;
