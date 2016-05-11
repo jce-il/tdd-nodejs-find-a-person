c6fb4454ea39af8821ae5e32a4cf9407e1064c52
OriAmir <oriamir1@gmail.com>
2016-05-11 20:02:46 +0300

    RED: Test for mult of 2

--- src/map.js
+++ src/map.js
@@ -7,4 +7,10 @@ Map.prototype.find_a_person = function(name) {
   return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
   };
 
+Map.prototype.multiply=function(num)
+{
+    return 1;
+    
+};
+
 module.exports = Map;
