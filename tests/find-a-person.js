cd4ffdf6155e6e53ba10aca6c36f51dbd7c4361b
flashxyz <flashxyz@gmail.com>
2016-05-11 16:30:21 +0200

    RED : added find location test

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,3 +9,12 @@ describe('Find a person', function() {
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 }); 
+describe('check location', function() {
+  it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var answer = map.check_for_location("Or A.")
+
+    //comment
+    expect(answer).to.be.eql("True");
+  });
+});
