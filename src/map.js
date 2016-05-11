27f609e32855d1723cc5f7c1f35319ca785f1618
flashxyz <flashxyz@gmail.com>
2016-05-11 16:35:23 +0200

    GREEN: function for finding different locations was added.

--- src/map.js
+++ src/map.js
@@ -37,4 +37,50 @@ Map.prototype.check_for_location = function(name) {
 	return "False";
 };
 
+Map.prototype.check_for_map_inconsistencies = function(name) {
+
+	var posts = this.find_a_person(name);
+	var indexOfAt = 0;
+	var places = [];
+	var numOfPlaces = 0;
+
+	for(i = 0; i < posts.length; i++)
+	{
+		indexOfAt = posts[i].indexOf("at");
+		if(indexOfAt > -1)
+		{
+			var splitedPost = posts[i].split(" "); 
+			for (var j = 0 ; j < splitedPost.length; j++)
+			{
+				if(splitedPost[j] == "at")
+				{
+					if( (j+1 < splitedPost.length) &&  splitedPost[j+1][0] === splitedPost[j+1][0].toUpperCase() ) 
+					{
+						places.push(splitedPost[j+1]);
+						numOfPlaces++;
+
+					}
+				}
+			}
+
+				
+		}
+	}
+		//check for multiple locations
+		if(numOfPlaces > 1)
+		{
+			for (var i = 0 ; i < numOfPlaces ; i++)
+			{
+				for (var j = 0 ; j < numOfPlaces ; j++)
+				{
+					if(places[i] != places[j])
+					{
+						return "True";
+					}
+				}
+			}
+		}
+
+		return "False";
+}
 module.exports = Map;
