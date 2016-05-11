09489393b4b60542c451421b2167cd80ba7b7836
OriAmir <oriamir1@gmail.com>
2016-05-11 19:44:24 +0300

    RED: Test for find a person

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -2,6 +2,8 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
 
+//Test num 1 : find a person
+
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
