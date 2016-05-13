178305ad8135a780926e1c32f8ca3d6c868a64cd
OriAmir <oriamir1@gmail.com>
2016-05-13 11:03:36 +0300

    RED: Test for find a car

--- src/map.js
+++ src/map.js
@@ -3,14 +3,20 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
+Map.prototype.find_a_person = function(name)
+{
   return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
-  };
+};
 
 Map.prototype.multiply=function(num)
 {
     return num*2;
-    
+};
+
+
+Map.prototype.find_a_car = function(name)
+{
+  return [];
 };
 
 module.exports = Map;
