72722b6d957e4484097277e6decd65637c35a54b
Buzz2000 <bazibox@gmail.com>
2016-05-15 21:19:14 +0300

    The first function i complete, all tests pass.

--- src/map.js
+++ src/map.js
@@ -3,8 +3,12 @@ function Map(posts) {
   this._posts = posts;
 }
 
+
+
 Map.prototype.find_a_person = function(name) {
-  return [];
+
+
+	return this._posts.filter(function(string){return string.indexOf(name) != -1});
   };
 
 module.exports = Map;
