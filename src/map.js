2a6054febf94fb966927be088afa907f3eb488dc
yuval <yuval66ts@gmail.com>
2016-05-15 13:29:29 +0300

    RED:find Or

--- src/map.js
+++ src/map.js
@@ -4,22 +4,7 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-	
-	var arrayLength = maps.length;
-	var array[];
-	var j = 0;
-	for (var i = 0; i < arrayLength; i++) 
-	{
-		for(var a = 0; a < maps[i].length - 4; a++)
-		{
-			if(maps[i][a] == 'O' && maps[i][a+1] == 'r' && maps[i][a+2] == ' ' && maps[i][a+3] == 'A')
-			{
-				array[j] = maps[i];
-				j++;
-			}
-		}
-	}
-  return array;
+  return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
   };
 
 module.exports = Map;
