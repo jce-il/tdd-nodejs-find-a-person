50b2d2bc1bd1984d5eb231e3738c4d5514292756
noyBarak <noy.brk@gmail.com>
2016-05-15 12:24:26 +0300

    initial find a person

--- src/map.js
+++ src/map.js
@@ -3,8 +3,9 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+Map.prototype.find_a_person = function("Or A.") 
+{
+  return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
+};
 
 module.exports = Map;
