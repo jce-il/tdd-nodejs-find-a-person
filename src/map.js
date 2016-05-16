a537ed871c8a0ba0b43ddedd4c035f24992c67ca
alonshmilo <alonshmilo@gmail.com>
2016-05-16 13:41:07 +0300

    GREEN: A test for a location

--- src/map.js
+++ src/map.js
@@ -5,7 +5,12 @@ function Map(posts) {
 
 Map.prototype.find_a_person = function(name) {
   return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
-  };
+
+ };
+ 
+ Map.prototype.find_location = function(name){
+ 	return true;
+ }; 
 
 
 
