dbd45cbb6eb95d90801b22b62d3dbe89c135772d
sarah-hoter <sarah.hoter@gmail.com>
2016-05-16 21:16:17 +0300

    GREEN: Test for finding a person that does not exist

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -18,6 +18,14 @@ describe('Find a person with shorter name', function() {
   });
 });
 
+describe('Find a person that does not exist', function() {
+  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post", "Or in Jerusalem"]);
+    var posts = map.find_a_person("AAA")
+    expect(posts).to.be.eql([]);
+  });
+});
+
 
 describe('Find a person with location', function() {
   it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
