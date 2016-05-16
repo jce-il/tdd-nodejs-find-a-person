8ab8a68eada235b44672b727dbfa3859ef76ee4b
Think-Smart <info@thinksmart.co.il>
2016-05-16 21:44:30 +0300

    GREEN: fix find by location

--- src/map.js
+++ src/map.js
@@ -8,7 +8,7 @@ Map.prototype.find_a_person = function(name) {
 };
 Map.prototype.find_by_location = function(name) {
 	if(this.find_a_person(name).length>0)
-		return false;
-	else return true;
+		return true;
+	else return false;
 };
 module.exports = Map;
