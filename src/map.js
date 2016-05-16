4dc40b8a279e7f6f725f0c0781c50168b1c51e29
alonaya <alonayag@gmail.com>
2016-05-16 16:00:02 +0300

    RED: check inconsistencies map

--- src/map.js
+++ src/map.js
@@ -1,17 +1,21 @@
 
 function Map(posts) {
-  this._posts = posts;
+	this._posts = posts;
 }
 
 Map.prototype.find_a_person = function(name) {
-  return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
-  };
+	return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
+};
   
 Map.prototype.find_by_location = function(name) {
-	if(this.find_a_person(name).length>0)
+	if(this.find_a_person(name).length > 0)
 		return true;
 	else 
 		return false;
 };
+
+Map.prototype.check_inconsistencies_map = function(name) {
+	return false;
+}
 
 module.exports = Map;
--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -4,9 +4,9 @@ var Map = require('./../src/map');
 
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
-    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.find_a_person("Or A.")
-    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+		var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+		var posts = map.find_a_person("Or A.")
+		expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 });
 
@@ -14,6 +14,14 @@ describe('Find by location', function() {
 	it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
 		var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
 		var posts = map.find_by_location("Or A.")
+		expect(posts).to.be.eql(true);
+	});
+});
+
+describe('Check inconsistencies map', function() {
+	it('Check if there are map inconsistencies, e.g., the same name with different locations', function() {
+		var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+		var posts = map.check_inconsistencies_map("Or A.")
 		expect(posts).to.be.eql(true);
 	});
 });
