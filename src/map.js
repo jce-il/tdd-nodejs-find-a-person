f72bd39f808149ad6f70ac6a2a267b7a8013bb02
sarah-hoter <sarah.hoter@gmail.com>
2016-05-16 21:22:37 +0300

    GREEN: supported "in" keyword to specify location, in addition to the "at" keyword used before

--- src/map.js
+++ src/map.js
@@ -13,7 +13,7 @@ Map.prototype.find_a_person = function(name) {
 
 Map.prototype.find_a_person_with_location = function(name) {
 	for (var i=0; i<this._posts.length; i++)
-		if (this._posts[i].indexOf(name) >= 0 && this._posts[i].indexOf(" at ") >= 0)
+		if (this._posts[i].indexOf(name) >= 0 && (this._posts[i].indexOf(" at ") >= 0 || this._posts[i].indexOf(" in ") >= 0))
 			return true;
 
 	return false;
@@ -25,8 +25,8 @@ Map.prototype.check_inconsistencies = function() {
 		//split into words in order to find names
 		var post = this._posts[i].split(" ");
 		
-		//find person names until "at" keyword
-		for (var j=0; j<post.length && post[j] != "at"; j++) {
+		//find person names until "at" or "in" keywords
+		for (var j=0; j<post.length && post[j] != "at" && post[j] != "in"; j++) {
 			//if a name exists more than once return true
 			if (this.find_a_person(post[j]).length > 1)
 				return true;
