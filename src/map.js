804615d1e17ec2ac69273ea67325398e0d00c0e0
navacohen90 <navacohen90@gmail.com>
2016-05-16 23:04:47 +0300

    GREEN: correct the code for test3 - add support to location that not in the list

--- src/map.js
+++ src/map.js
@@ -40,10 +40,18 @@ Map.prototype.check_map_inconsistencies = function() {
 		var person_locations = cmap.find_a_person(name);
 
 		person_locations.forEach(function(value) {
+			var flag = false;
 			locations.forEach(function(location) {
-				if (value.indexOf(location) > -1)
+				if (value.indexOf(location) > -1) {
 					array.push(location);	
+					flag = true;
+				}
 			});
+			if (!flag) {
+				if (value.indexOf("at ") > -1) {
+					array.push(value.substr(value.indexOf("at ") + 3));
+				}
+			}
 		});
 
 		if (array.length > 1) {
