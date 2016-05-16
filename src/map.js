da13653eac29cbf7ce3d57be22cf0e87ea1efbe5
talshi <talshiran0000@gmail.com>
2016-05-16 21:34:18 +0300

    RED: test for finding a location by name

--- src/map.js
+++ src/map.js
@@ -6,5 +6,9 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
   return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
   };
+  
+Map.prototype.find_a_location = function(name) {
+
+};
 
 module.exports = Map;
