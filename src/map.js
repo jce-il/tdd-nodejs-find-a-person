a2c64f73ccea282c21584aed06ef989c179da624
rotemsd <saadorotem@gmail.com>
2016-05-16 22:23:11 +0300

    GREEN: fix find a person function

--- src/map.js
+++ src/map.js
@@ -4,7 +4,17 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
+  
+	var res = [];
+ 	var i;
+ 	for(i = 0; i < this._posts.length; i++)
+ 	{
+ 		if(this._posts[i].indexOf(name) > -1)
+ 		{
+ 			res.push(this._posts[i]);
+ 		}
+ 	}
+ 	return res;
   };
 
 module.exports = Map;
