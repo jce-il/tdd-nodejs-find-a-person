116f9374c83b2ad407cce46e8d8933de35663388
Dor-H <dorharel1@hotmail.com>
2016-05-16 13:56:10 +0300

    RED: find by location

--- src/map.js
+++ src/map.js
@@ -6,5 +6,11 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
   return ["I met "+ name +" at Chabad house Bangkok", "We found " +name+" R.I.P at Langtang valley"];
   };//Dor-H
-
+Map.prototype.find_by_location = function(name) {
+  if(this.find_a_person(name).length>0)
+ return false;
+ 	else 
+  return true;
+ 	
+   };
 module.exports = Map;
