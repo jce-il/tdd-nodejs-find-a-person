b6130fef995613d0eb6c529ef4127c8b9152dfa5
sarah-hoter <sarah.hoter@gmail.com>
2016-05-16 20:46:21 +0300

    GREEN: implement find_a_person_with_location function

--- src/map.js
+++ src/map.js
@@ -12,6 +12,10 @@ Map.prototype.find_a_person = function(name) {
 };
 
 Map.prototype.find_a_person_with_location = function(name) {
+	for (var i=0; i<this._posts.length; i++)
+		if (this._posts[i].indexOf(name) >= 0 && this._posts[i].indexOf(" at ") >= 0)
+			return true;
+
 	return false;
 };
 
