6edf20745a1c522ab5788ebebf894157573d1dbc
sarah-hoter <sarah.hoter@gmail.com>
2016-05-16 20:33:30 +0300

    GREEN: implement find_a_person function

--- src/map.js
+++ src/map.js
@@ -4,7 +4,11 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+	var arr = [];
+	for (var i=0; i<this._posts.length; i++)
+		if (this._posts[i].indexOf(name) >= 0)
+			arr.push(this._posts[i]);
+	return arr;
+};
 
 module.exports = Map;
