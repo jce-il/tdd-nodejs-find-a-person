590dcdd21f942c3dc3d62d91907b6752b49c0b4b
navacohen90 <navacohen90@gmail.com>
2016-05-16 22:49:47 +0300

    GREEN: implement the code for test check map inconsistencies

--- src/map.js
+++ src/map.js
@@ -1,4 +1,5 @@
 var locations = ["Israel", "Bangkok", "Langtang valley"];
+var names = ["Or A.", "Or B.", "blabla"];
 
 function Map(posts) {
   this._posts = posts;
@@ -30,8 +31,30 @@ Map.prototype.find_a_person = function(name) {
 
   
 Map.prototype.check_map_inconsistencies = function() {
+	var res = false;	
 	
-	return null;
+	var cmap = new Map(this._posts);
+	names.forEach(function(name) {
+		var array = [];	
+		
+		var person_locations = cmap.find_a_person(name);
+
+		person_locations.forEach(function(value) {
+			locations.forEach(function(location) {
+				if (value.indexOf(location) > -1)
+					array.push(location);	
+			});
+		});
+
+		if (array.length > 1) {
+			var loc = array[0];
+			for (var i = 1; i < array.length; i++) {
+				if (loc != array[i])
+					res = true;
+			}
+		}	
+	});
+	return res;
   };
   
 module.exports = Map;
