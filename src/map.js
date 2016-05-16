6d16902256a4b387c59c0e64247d25c490d1cd81
navacohen90 <navacohen90@gmail.com>
2016-05-16 23:09:21 +0300

    REFACTOR: refactor for test2 - reuse function find_a_person

--- src/map.js
+++ src/map.js
@@ -16,14 +16,16 @@ Map.prototype.find_a_person = function(name) {
 
  Map.prototype.check_if_person_in_location = function(name) {
 	var res = false;	
-	this._posts.forEach(function(value) {
-		if (value.indexOf(name) > -1 ) {	
+	var cmap = new Map(this._posts);
+	var person_locations = cmap.find_a_person(name);
+
+	//go over person posts and check if at least one of them is with location
+	person_locations.forEach(function(value) {
 			locations.forEach(function(location) {
 				if (value.indexOf(location) > -1)
 					res = true;	
 			});
 			res =  res || (value.indexOf(" at ") > -1)
-		}
   		
 	}); 	
 	return res;
@@ -32,13 +34,15 @@ Map.prototype.find_a_person = function(name) {
   
 Map.prototype.check_map_inconsistencies = function() {
 	var res = false;	
-	
 	var cmap = new Map(this._posts);
+	//go over all the possible names
 	names.forEach(function(name) {
 		var array = [];	
 		
+		//for each name find all its locations
 		var person_locations = cmap.find_a_person(name);
 
+		//add the locations for specific name to an array
 		person_locations.forEach(function(value) {
 			var flag = false;
 			locations.forEach(function(location) {
@@ -54,6 +58,7 @@ Map.prototype.check_map_inconsistencies = function() {
 			}
 		});
 
+		//check if in the array of locations of specific name there is duplication 
 		if (array.length > 1) {
 			for (var i = 0; i < array.length; i++) {
 				for (var j = 0; j < array.length; j++) {
