faf01f37b3709231dd075d41a33012cb4b8ef700
alonaya <alonayag@gmail.com>
2016-05-16 15:48:42 +0300

    RED: find by location

--- src/map.js
+++ src/map.js
@@ -6,5 +6,12 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
   return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
   };
+  
+Map.prototype.find_by_location = function(name) {
+	if(this.find_a_person(name).length>0)
+		return false;
+	else 
+		return true;
+};
 
 module.exports = Map;
