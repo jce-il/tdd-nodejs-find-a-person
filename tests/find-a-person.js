e45e1edb02a04d122136d9e8bb85e47a131cc39f
sarah-hoter <sarah.hoter@gmail.com>
2016-05-16 21:15:05 +0300

    GREEN: Test for finding a person with shorter name

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -10,6 +10,14 @@ describe('Find a person', function() {
   });
 });
 
+describe('Find a person with shorter name', function() {
+  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post", "Or in Jerusalem"]);
+    var posts = map.find_a_person("Or")
+    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Or in Jerusalem"]);
+  });
+});
+
 
 describe('Find a person with location', function() {
   it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
