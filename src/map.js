0abaf5d3f9d26c7cbba1dcaeda6f5e4c6a853d1d
shani joffe <shanijoffe319189@gmail.com>
2016-05-16 19:49:14 +0300

    RED:error in number returned

--- src/map.js
+++ src/map.js
@@ -4,7 +4,16 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
+   
+    
+        return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
+        
+  };
+Map.prototype.powNum = function(num) {
+   
+    
+        return num;
+        
   };
 
 module.exports = Map;
--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -2,10 +2,15 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
 
-describe('Find a person', function() {
+describe('Find a person', function() { //green
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.find_a_person("Or A.")
+    var posts = map.find_a_person("Or A")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+  });
+    it('given a number ,return the power of the number', function() {
+    var map = new Map([2,3,4,7,8,16]);
+    var posts = map.powNum(2)
+    expect(posts).to.be.eql(4);
   });
 });
