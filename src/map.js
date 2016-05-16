9478c3315739921f5ebbe7c2ab51474f5b57ae98
alonshmilo <alonshmilo@gmail.com>
2016-05-16 13:38:43 +0300

    RED: A test for a location

--- src/map.js
+++ src/map.js
@@ -7,4 +7,6 @@ Map.prototype.find_a_person = function(name) {
   return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
   };
 
+
+
 module.exports = Map;
