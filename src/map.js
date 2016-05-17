e4c5bc195986a01539281c1053f6a7e5a481f19f
Gyputer <gy.puter@gmail.com>
2016-05-17 19:40:40 +0300

    GREEN: Test for finding a person who dos not exist

--- src/map.js
+++ src/map.js
@@ -3,9 +3,8 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-	
-  return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
+Map.prototype.find_a_person = function(name) {	
+  return [];
   };
 
 module.exports = Map;
