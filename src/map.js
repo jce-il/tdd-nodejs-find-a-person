848b9fb8fffe6b592fa2d34081ba0ab7b56580c9
Gyputer <gy.puter@gmail.com>
2016-05-17 19:47:54 +0300

    GREEN: finisshd the first function

--- src/map.js
+++ src/map.js
@@ -3,8 +3,16 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {	
-  return [];
+Map.prototype.find_a_person = function(name) {
+	function find_substring(string, sub_string){
+	if(string.indexOf(name)!=-1)
+		return true;
+	else
+		return false
+
+}
+	return this._posts.filter(find_substring);
+  
   };
 
 module.exports = Map;
