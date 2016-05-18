e0d423b08410795a84a167f6c35ae13e0006f4a8
Tomer Achdut <tomera90@gmail.com>
2016-05-18 19:42:43 +0300

    RED: A test for a location

--- src/map.js
+++ src/map.js
@@ -5,7 +5,9 @@ function Map(posts) {
 
 Map.prototype.find_a_person = function(name) {
   return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
-  };
+};
 
-  
+Map.prototype.find_location = function(name){
+	return false;
+}; 
 module.exports = Map;
--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,4 +9,11 @@ describe('Find a person', function() {
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 
+    
+  it('Given a name, check if the map includes a location information for it (a place or geo. location)',function(){
+  	var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+  	var loc = map.find_location("or A.")
+  	expect(loc).to.be.eql(true);
+  	});
+	
 });
