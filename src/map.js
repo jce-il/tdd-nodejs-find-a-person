ff1d4c164be74622a238bc5f958f886b63876b91
Afek H <afek590@gmail.com>
2016-05-18 14:17:48 +0300

    Green: mupltily passes

--- src/map.js
+++ src/map.js
@@ -6,5 +6,10 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
   return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
   };
+  
+ 
+Map.prototype.multiply = function(num){
+	return num*2;
+}
 
 module.exports = Map;
