6b85826ecb7cd25b1895188bfe879ddd0f076ac9
miriyak <miriyakobi@gmail.com>
2016-05-19 15:27:08 +0300

    Red - Test the implementation by another person

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,3 +9,12 @@ describe('Find a person', function() {
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 });
+
+describe('Find a person - check another', function () {
+    it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function () {
+        var map = new Map(["I met Miri at Chabad house Bangkok", "We found Miri R.I.P at Langtang valley", "Random post"]);
+        var posts = map.find_a_person("Random")
+        expect(posts).to.be.eql(["I met Miri at Chabad house Bangkok", "We found Miri R.I.P at Langtang valley"]);
+        expect(posts).to.be.eql(["Miri"]);
+    });
+});
