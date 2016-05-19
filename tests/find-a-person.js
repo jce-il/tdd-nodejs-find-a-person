544e47bebfcfde0726ab151a9503e93a3715d418
Shon Otmazgin <shon711@gmail.com>
2016-05-19 15:50:54 +0300

    RED: Test for finding a location info

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,4 +9,9 @@ describe('Find a person', function() {
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
+it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var location_exsit = map.find_a_location("Or A.")
+    expect(location_exsit).to.be.eql(true);
+  });
 });
