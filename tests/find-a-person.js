4f2dfab02d4388f7378ea1ed3b67909d8f6aa836
miriyak <miriyakobi@gmail.com>
2016-05-19 15:37:18 +0300

    GREEN - Test an empty array result - by find not existing person

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -17,3 +17,11 @@ describe('Find a person - check another', function () {
         expect(posts).to.be.eql(["I met Miri at Chabad house Bangkok", "We found Miri R.I.P at Langtang valley"]);
     });
 });
+describe('Find a person - check an empty', function () {
+    it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function () {
+        var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+        var posts = map.find_a_person("Miri")
+        expect(posts).to.be.eql([]);
+    });
+});
+
