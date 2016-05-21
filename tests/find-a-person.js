cd5f08b5981c2cf1dfbc98c838d38c131aeb6d83
oron sason <oron sason>
2016-05-21 14:43:13 +0300

    RED: Test for is_inconsistencies

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -15,5 +15,13 @@ describe('Find a person', function() {
       var posts = map.find_by_loc("Or A.")
       expect(posts).to.be.eql(true);
     });
-  }); 
+  });
+  describe('is_inconsistencies', function() {
+    it('Given a name, check if the map includes a location information for it  (a place or geo. location)', function() {
+      var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+      var posts = map.is_inconsistencies("Or A.")
+      expect(posts).to.be.eql(true);
+    });
+  });
+
 });
