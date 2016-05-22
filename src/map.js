83c4c57959161abf06df63983cf45a1fff3562d7
ofirnagadi <ofirnagadi@gmail.com>
2016-05-22 21:59:09 +0300

    GREEN: Test for finding a name

--- src/map.js
+++ src/map.js
@@ -4,7 +4,14 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+    var post=[];
+    for(var i=0; i< this._posts.length; i++)
+    {
+        if(this._posts[i].search(name) >= 0)
+           post.push(this._posts[i]);
+    }
+    return post;
+};
+      
 
 module.exports = Map;
