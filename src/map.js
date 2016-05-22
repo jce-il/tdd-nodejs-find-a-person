6fc9358e5c6668b9dafdaabdfd40efdccde797f8
hudeda <hudeda641@gmail.com>
2016-05-22 20:49:50 +0300

    GREEN: find a person fixed

--- src/map.js
+++ src/map.js
@@ -1,10 +1,18 @@
-//Adam hudeda 201570793
+//Adam hudeda 201570793 
 function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return [];
+Map.prototype.find_a_person = function(person) {
+  
+	var results = [];
+ 	var i;
+ 	for(i = 0;i < this._posts.length; i++){
+ 		if(this._posts[i].indexOf(person) > -1){
+ 			results.push(this._posts[i]);
+ 		}
+ 	}
+ 	return results;
   };
 
 module.exports = Map;
