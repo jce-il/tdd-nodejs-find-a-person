2777af253af95f8df05d960f6c0dc49e7be9d441
hudeda <hudeda641@gmail.com>
2016-05-22 21:43:57 +0300

    GREEN: pemove posts by location fixed

--- src/map.js
+++ src/map.js
@@ -1,36 +1,35 @@
-//Adam hudeda 201570793 
 function Map(posts) {
   this._posts = posts;
 }
 
 Map.prototype.find_a_person = function(person) {
-  
-	var results = [];
- 	var i;
- 	for(i = 0;i < this._posts.length; i++){
- 		if(this._posts[i].indexOf(person) > -1){
- 			results.push(this._posts[i]);
- 		}
- 	}
- 	return results;
-  };
-
-  
-Map.prototype.check_for_location = function(person){
- 
- 	return true;};
-	
-	
-Map.prototype.isInconsistencies = function(person) {
-    if(this.find_a_person(person).length > 1)
-        return true;
-     return false;
- };
- 
-Map.prototype.remove_a_posts_by_name = function(person) {
-    return false; 
+    var posts = [];
+    for(var i =0; i<this._posts.length; i++)
+        if(this._posts[i].search(person) >= 0)
+            posts.push(this._posts[i]);
+    return posts;    
 };
 
- 
-  
+Map.prototype.find_a_location = function(person) {
+    for(var i =0; i<this._posts.length; i++)
+        if(this._posts[i].search(person) >= 0)
+            return true;
+    return false;
+};
+
+Map.prototype.isInconsistencies = function(person) {
+    if(this.find_a_person(person).length >= 2)
+        return true;
+    return false;
+};
+
+Map.prototype.remove_a_posts_by_name = function(person) {
+    var posts = [];
+    for(var i =0; i<this._posts.length; i++)
+        if(this._posts[i].search(person) < 0)
+            posts.push(this._posts[i]);
+    this._posts = posts;
+    return posts; 
+};
+
 module.exports = Map;
