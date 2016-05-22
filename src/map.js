2ddf2a153d0cee4441bbbbec1d9ba67aa675f358
ofirnagadi <ofirnagadi@gmail.com>
2016-05-22 22:07:28 +0300

    GREEN: Test for finding a location

--- src/map.js
+++ src/map.js
@@ -13,7 +13,15 @@ Map.prototype.find_a_person = function(name) {
     return post;
 };
 
-
+Map.prototype.find_a_location = function(name) {
+    var post=[];
+    for(var i=0; i< this._posts.length; i++)
+    {
+        if(this._posts[i].search(name) >= 0)
+           return true;
+    }
+    return false;
+};
       
 
 module.exports = Map;
