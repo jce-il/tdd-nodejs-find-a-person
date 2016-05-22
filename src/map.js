30ec889bf41a0abcd79670c75dd21a18b4b7aecd
ofirnagadi <ofirnagadi@gmail.com>
2016-05-22 22:29:36 +0300

    GREEN: Test for person in location

--- src/map.js
+++ src/map.js
@@ -34,6 +34,16 @@ Map.prototype.is_consistencies = function(name) {
         return true;
     return false;
 };
+
+Map.prototype.in_location = function(name, location) {
+    var post=[];
+    for(var i=0; i< this._posts.length; i++)
+    {
+        if(this._posts[i].search(name) >= 0 && this._posts[i].search(location) >= 0)
+           return true;
+    }
+    return false;
+};
       
 
 module.exports = Map;
