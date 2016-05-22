5fdf6c2c7ed80c4826cd907faef285ac022f07e2
Gal Zaken <Gal Zaken>
2016-05-22 15:55:56 +0300

    RED: Test for finding a inconsistencies

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -1,17 +1,26 @@
-// gal zaken
+// Gal Zaken
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
 
 describe('Find a person', function() {
+    
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
-    it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
-        var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-        var location = map.find_location("Or A.")
-        expect(location).to.be.eql(true);
-    });
+    
+  it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var location = map.find_location("Or A.")
+    expect(location).to.be.eql(true);
+  });
+    
+  it('Check if there are map inconsistencies the same name with different locations', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var inconsistencies = map.isInconsistencies("Or A.");
+    expect(inconsistencies).to.be.eql(true);
+  });
+    
 });
