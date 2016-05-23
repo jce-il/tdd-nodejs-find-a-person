d180b94e926ddc4fff21e4653eaceb03b268f90e
Arye Kogan <aryekogan@gmail.com>
2016-05-23 15:19:41 +0300

    Red: Finding inconsistencies

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -14,4 +14,9 @@ it('Given a name, check if the map includes a location information for it (a pla
     var location_exsit = map.find_a_location("Or A.");
     expect(location_exsit).to.be.eql(true);
   });
+it('Check if there are map inconsistencies, e.g., the same name with different locations', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var isInconsistencies = map.isInconsistencies("Or A.");
+    expect(isInconsistencies).to.be.eql(true);
+  });
 });
