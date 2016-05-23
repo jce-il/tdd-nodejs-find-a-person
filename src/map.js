d81b028dd81405af61a34984c28833ee7c1db689
Gyputer <gy.puter@gmail.com>
2016-05-23 08:19:22 +0300

    GREEN: implement find_a_person_with_location function

--- src/map.js
+++ src/map.js
@@ -14,6 +14,13 @@ Map.prototype.find_a_person = function(name) {
 	return this._posts.filter(find_substring);
   
   };
+  
+Map.prototype.find_a_person_with_location = function(name) {
+ if (this._posts[i].indexOf(name) >= 0 && this._posts[i].indexOf(" at ") >= 0)
+ 			return true;
+ 
+  	return false;
+  };  
 
 module.exports = Map;
 
