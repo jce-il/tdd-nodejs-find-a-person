852e9ca8617473b136d69bf7d6146d4fc49326af
AlumaK <aluma.ketz@gmail.com>
2016-05-23 19:52:32 +0300

    GREEN: Test for finding a name

--- src/map.js
+++ src/map.js
@@ -4,7 +4,13 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [ ];
-  };
+	var posts_contain_person_name = [];
+	for(i=0; i<this._posts.length; i++)
+	{
+		if(this._posts[i].includes(name))
+			posts_contain_person_name.push(this._posts[i]);
+	}
+	return posts_contain_person_name;
+};
 
 module.exports = Map;
