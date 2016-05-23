dc6459b0e8128fdce8de7be37cd9b0c84eff75db
anisbi <anis.bishara1@gmail.com>
2016-05-23 15:22:47 +0300

    GREEN: Map inconsist test #1

--- src/map.js
+++ src/map.js
@@ -20,4 +20,15 @@ Map.prototype.name_matches = function(name) {
 	return false;
 };
 
+Map.prototype.map_inconsist = function(name) {
+	var counter = 0;
+	var lngth = this._posts.length;
+	for (i=0; i<lngth; i++) {
+		if (this._posts[i].search(name) != -1)
+			counter++;
+		if (counter > 1)
+			return true;
+	}
+	return false;
+};
 module.exports = Map;
--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
 
-var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "We met Amit B. in NYC"]);
 
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
@@ -20,8 +20,9 @@ describe('Name match found (bool)', function() {
 
 
 describe('Map inconsistencies test #1', function() {
-  it('heck if there are map inconsistencies, e.g., the same name with different locations.', function() {
+  it('Check if there are map inconsistencies, e.g., the same name with different locations.', function() {
     var posts = map.map_inconsist("Or A.")
     expect(posts).to.be.eql(true);
   });
 });
+
