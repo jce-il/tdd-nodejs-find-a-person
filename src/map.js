18cf652e66feec22263ab82c4e23cd852963b786
HosamKhatib <hosam.kh@live.com>
2016-05-23 20:40:03 +0300

    RED: 'Name match found' test

--- src/map.js
+++ src/map.js
@@ -11,5 +11,13 @@ Map.prototype.find_a_person = function(name) {
    }
    return toReturn;
   };
+  
+ Map.prototype.name_matches = function(name) {
+ 	for (i = 0; i<this._posts.length; i++) {
+ 		if (this._posts[i].search(name) != -1)
+ 			return true;	
+ 	}
+ 	return false;
+ };
 
 module.exports = Map;
