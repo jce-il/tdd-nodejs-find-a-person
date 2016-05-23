8cd103fc7970412e4f29ec97c24244ee991a0e63
TAREQ <tareqsaeed01@gmail.com>
2016-05-23 18:19:09 +0300

    TEST: new test

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -4,7 +4,7 @@ var Map = require('./../src/map');
 
 var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
 describe('Find a person', function() {
-  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
@@ -16,3 +16,9 @@ describe('Name match (bool)', function() {
  		expect(posts).to.be.eql(true);
  	});
  });
+ describe('Map inconsistencies first test', function() {
+    it('check if there are map inconsistencies (different location) ', function() {
+     var posts = map.map_inconsist("Or A.")
+     expect(posts).to.be.eql(true);
+   });
+ }); 
