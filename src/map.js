0bae40058301d72f01bde20b39cd734d240b534f
amitay ben atar <amitayben@gmail.com>
2016-05-23 15:03:41 +0300

    running test check' authour name and id

--- src/map.js
+++ src/map.js
@@ -1,19 +1,12 @@
-function Map(posts) {
-  this._posts = posts;
-}
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var Map = require('./../src/map');
 
-Map.prototype.find_a_person = function(name) {
+describe('Find a person', function() {
+  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var posts = map.find_a_person("Or A.")
+    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+  });
 
-  var res = [];
-  var i;
-  for(i = 0; i < 3; i++)
-  {
-    if(this._posts[i].indexOf(name) > -1)
-    {
-      res.push(this._posts[i]);
-    }
-  }
-  return res;
-};
-
-module.exports = Map;
+});
