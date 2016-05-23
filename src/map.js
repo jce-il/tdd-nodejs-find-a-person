16965570aadf729fa123b46959f7d9920a6423f8
stavdv <stavbarazani@gmail.com>
2016-05-23 12:02:08 +0300

    RED: Test for  finiding a location and  Check if there are map inconsistencies

--- src/map.js
+++ src/map.js
@@ -8,10 +8,10 @@ Map.prototype.find_a_person = function(name) {
 };
 
 Map.prototype.find_location = function(name){
-	return true;
+	return false;
 }; 
 Map.prototype.find_inconsistencies = function(name){
-	return true;
+	return false;
 };
 Map.prototype.find_any_collaboration = function(name){
 	return true;
