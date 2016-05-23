08b7b868af79959909f932d71ec6471521e4b58f
amitay ben atar <amitayben@gmail.com>
2016-05-23 15:06:29 +0300

    GREEN: Test for finding a name

--- src/map.js
+++ src/map.js
@@ -1,9 +1,13 @@
-this._posts = posts;
+function Map(posts) {
+  this._posts = posts;
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
-
+  var posts = [];
+  for(var i =0; i<this._posts.length; i++)
+    if(this._posts[i].search(name) > -1)
+      posts.push(this._posts[i]);
+  return posts;
 };
 
 module.exports = Map;
