c28cd217ffa1da9d381d0601a87b3c997cedba71
hadiab <abdalhadi.m92@gmail.com>
2016-05-23 15:26:36 +0300

    RED: Test for finding a name

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -2,10 +2,18 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
 
-describe('Find a person', function() {
-  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+describe('Find by location', function() {
+  it('Given a name, check if the map includes a location information for it  (a place or geo. location)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.find_a_person("Or A.")
-    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
-  });
+    var posts = map.find_by_loc("Or A.")
+    expect(posts).to.be.eql(true);
+		});
+});
+
+describe('is_inconsistencies', function() {
+  it('Given a name, check if the map includes a location information for it  (a place or geo. location)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var posts = map.is_inconsistencies("Or A.")
+    expect(posts).to.be.eql(true);
+		});
 });
