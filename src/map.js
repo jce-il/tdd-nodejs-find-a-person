28a9f5d9dfb9c842334f203c0cf02205b1351219
amitay ben atar <amitayben@gmail.com>
2016-05-23 15:05:25 +0300

    RED: finding a name test

--- src/map.js
+++ src/map.js
@@ -1,12 +1,9 @@
-var chai = require('chai');
-var expect = chai.expect; // we are using the "expect" style of Chai
-var Map = require('./../src/map');
+this._posts = posts;
+}
 
-describe('Find a person', function() {
-  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
-    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.find_a_person("Or A.")
-    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
-  });
+Map.prototype.find_a_person = function(name) {
+  return [];
 
-});
+};
+
+module.exports = Map;
