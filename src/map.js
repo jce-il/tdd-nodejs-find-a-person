dd34ac4e855578aef0a4e14d1943bb58b9e2b8f1
Itay Saidoff <itaysaidoff@gmail.com>
2016-05-23 14:56:53 +0300

    @nirbm
    git commit -m @nirbm

--- src/map.js
+++ src/map.js
@@ -5,9 +5,13 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
     var posts = [];
     for(var i =0; i<this._posts.length; i++)
-        if(this._posts[i].search(name) > -1)
+        if(this._posts[i].search(name) >= 0)
             posts.push(this._posts[i]);
     return posts;    
+};
+
+Map.prototype.find_a_location = function(name) {
+    return false;   
 };
 
 module.exports = Map;
