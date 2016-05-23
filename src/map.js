41fbadc6918f4df9a543a7592d695d30578278c3
anisbi <anis.bishara1@gmail.com>
2016-05-23 14:49:39 +0300

    RED: Name match found test

--- src/map.js
+++ src/map.js
@@ -10,6 +10,14 @@ Map.prototype.find_a_person = function(name) {
   		toReturn.push(this._posts[i])
   }
   return toReturn;
-  };
+};
+
+Map.prototype.name_matches = function(name) {
+	for (i = 0; i<this._posts.length; i++) {
+		if (this._posts[i].search(name) != -1)
+			return true;	
+	}
+	return false;
+};
 
 module.exports = Map;
