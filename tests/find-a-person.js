474550473d12a054beae849e234838ff57120ba2
Arye Kogan <aryekogan@gmail.com>
2016-05-23 15:16:08 +0300

    RED: Finding location information 2nd file

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -1,3 +1,4 @@
+// Arye Kogan, ID - 303925671
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
@@ -7,5 +8,10 @@ describe('Find a person', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+  });
+it('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var location_exsit = map.find_a_location("Or A.")
+    expect(location_exsit).to.be.eql(true);
   });
 });
