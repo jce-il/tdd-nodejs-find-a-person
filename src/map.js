3ab14f565de8d7d4fc15ef89ec9935708d3556e0
Monder <monder_1993@hotmail.com>
2016-05-23 20:08:55 +0300

    RED : founding matched names test

--- src/map.js
+++ src/map.js
@@ -12,4 +12,14 @@ Map.prototype.find_a_person = function(name) {
    return toReturn;
   };
 
+  Map.prototype.name_matches = function(name) {
+ 	for (i = 0; i<this._posts.length; i++) {
+ 		if (this._posts[i].search(name) != -1)
+ 			return true;	
+	}
+ 	return false;
+ };
+  
+  
+  
 module.exports = Map;
