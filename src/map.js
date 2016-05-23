f4c75e3337d70be1a267665d58c2f43f81558fee
MUHAMMED IBRAHEM <mohamdib@gmail.com>
2016-05-23 13:17:31 +0300

    GREEN: Test for finding a name

--- src/map.js
+++ src/map.js
@@ -6,5 +6,11 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
   return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
   };
+  
+Map.prototype.find_by_loc = function(name) {
+    if(this.find_a_person(name).length>0)
+		return true;
+	return false;
+};
 
 module.exports = Map;
