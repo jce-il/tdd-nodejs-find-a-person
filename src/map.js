dbc67a7001f1b279b1e97dd712657bd96b86d000
TAREQ <tareqsaeed01@gmail.com>
2016-05-23 17:55:11 +0300

    GREEN: find per passed

--- src/map.js
+++ src/map.js
@@ -5,7 +5,12 @@ function Map(posts) {
 
 Map.prototype.find_a_person = function(name) {
 	
-  return ["now red"];
+  var Ret = [];
+  for (i = 0; i<this._posts.length; i++) {
+  	if (this._posts[i].search(name) != -1)
+  		Ret.push(this._posts[i])
+  }
+  return Ret;
   };
 
 module.exports = Map;
