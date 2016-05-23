038c1690c5e9fc6140259fb97a04ddae6291c03b
anisbi <anis.bishara1@gmail.com>
2016-05-23 14:45:13 +0300

    Added new test (match name)

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -2,10 +2,18 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Map = require('./../src/map');
 
+var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
-    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
+});
+
+describe('Name match found (bool)', function() {
+	it ('Given a name, check if the map includes a location information for it (a place or geo. location)', function() {
+		var posts = map.find_a_person("Or A.")
+		expect(posts).to.be.eql(true);
+	});
 });
