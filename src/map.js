3f28b52eb365750fa2486b7c83d3241e0eaf6f34
Tom <ToOoM.M@gmail.com>
2016-05-23 20:03:13 +0300

    GREEN: find a person

--- src/map.js
+++ src/map.js
@@ -4,7 +4,18 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-	return [];
+	var i,j;
+	var length = this._posts.length;
+	var post = [];
+	
+	for(i = 0, j = 0; i < length; i++){
+		if (this._posts[i].search(name) !== -1){
+			post[j] = this._posts[i];
+			j++;
+		}
+	}
+	
+	return post;
 };
 
 module.exports = Map;
