3520e7a8d123bedfdf00fbb21249a4342503fbd3
Moria Tuchband <moriyatu@gmail.com>
2016-05-24 20:32:49 +0300

    Red: find person

--- src/map.js
+++ src/map.js
@@ -6,11 +6,10 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
   var result =[];
   for(i=0;i<this._posts.length;i++){
-    var n = this._posts[i].search(name);
-    if(n!=-1){
-      ret_val.push(this._posts[i]);
+    if(this._posts[i].search(name)>=0){
+      result.push(this._posts[i]);
     }
   }
-  return ret_val;
+  return result;
 };
 module.exports = Map;
