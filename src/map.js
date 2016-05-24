3dd9451231c8c777d8d0bca5fb8eed4c02e3a420
yuval <yuval66ts@gmail.com>
2016-05-24 12:06:38 +0300

    find RED:func

--- src/map.js
+++ src/map.js
@@ -4,7 +4,30 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
+	var ret_val =[]; 
+	var i=0;
+	for(i=0;i<this._posts.length;i++){
+		var n = this._posts[i].search(name);
+		if(n!=-1){
+			ret_val.push(this._posts[i]);
+		}
+	}
+	return ret_val;
+};
+  
+Map.prototype.find_by_location = function(name) {
+  return true;
   };
 
+  
+Map.prototype.find_inconsistencies = function(name){
+  	return true;
+};
+
+Map.prototype.find_any_collaboration = function(name){
+   	return true;
+};
+
 module.exports = Map;
+
+
