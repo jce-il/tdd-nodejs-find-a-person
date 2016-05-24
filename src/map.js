8e623ca7d886821869f0e6580cc2830a3ac7186a
shani joffe <shanijoffe319189@gmail.com>
2016-05-24 14:17:43 +0300

    RED: in function find a true

--- src/map.js
+++ src/map.js
@@ -9,6 +9,18 @@ Map.prototype.find_a_person = function(name) {
         return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
         
   };
+
+
+Map.prototype.find_a_true = function(name) {
+        return false;        
+  };
+
+Map.prototype.find_a_true2 = function(name) {
+        return true;        
+  };
+
+
+
 Map.prototype.powNum = function(num) {
    
         return num*num;  
@@ -18,16 +30,26 @@ Map.prototype.DecNum = function(num) {
    
         return num-2;  
   };
+
 Map.prototype.findNUM = function(num) {
    
         return num+10;  
   };
+
+
 Map.prototype.findNeg = function(num) {
    
         return num*-1;  
   };
+
 Map.prototype.findSum = function(num1,num2) {
    
         return num1+num2;  
   };
+
+Map.prototype.findmult = function(num1,num2) {
+   
+        return num1*num2;  
+  };
+  
 module.exports = Map;
--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -8,12 +8,39 @@ describe('Find a person', function() { //green
     var posts = map.find_a_person("Or A")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
+  
+    
+    
+   
+    
+ 
+  it('Given a person name, return true or false', function() {
+    var map = new Map([true,false]);
+    var posts = map.find_a_true("Or A")
+    expect(posts).to.be.eql(true);
+  });
+    
+   it('Given a place, return true or false if the place appears in 2 places ', function() {
+    var map = new Map([true,false]);
+    var posts = map.find_a_true2("nepal map")
+    expect(posts).to.be.eql(true);
+  });
+    
+ 
+  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    var map = new Map([true,false]);
+    var posts = map.find_a_true("Or A")
+    expect(posts).to.be.eql(true);
+  });
+  
+        
     it('given a number ,return the power of the number', function() {
     var map = new Map([2,3,4,7,8,16]);
     var posts = map.powNum(2)
     expect(posts).to.be.eql(4);
   });
     
+   
       it('given a number decrease 2 from it ', function() {
     var map = new Map([2,3,4,7,8,16]);
     var posts = map.DecNum(4)
@@ -21,18 +48,21 @@ describe('Find a person', function() { //green
           
   });
  
-     it('given a number find the number +10 ', function() {
+
+    it('given a number find the number +10 ', function() {
     var map = new Map([10,20,30,40]);
     var posts = map.findNUM(10)
     expect(posts).to.be.eql(20);
         
   });
+    
        it('given a number find its negative  ', function() {
     var map = new Map([-1,-2,-4]);
     var posts = map.findNeg(1)
     expect(posts).to.be.eql(-1);
         
   });
+    
     
          it('given 2 numbers ..return the sum between them ', function() {
     var map = new Map([3,6,7,8,6,20,22,9]);
@@ -41,10 +71,12 @@ describe('Find a person', function() { //green
         
   });
     
+    
            it('given 2 numbers ..return the devision between them ', function() {
-    var map = new Map([3,6,2,7,8,6,20,22,9]);
-    var posts = map.findDevision(6,3)
-    expect(posts).to.be.eql(2);
+    var map = new Map([3,6,2,7,8,6,20,22,9,18]);
+    var posts = map.findmult(6,3)
+    expect(posts).to.be.eql(18);
         
   });
+ 
 });
