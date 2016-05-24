9ca9345a83215f4b061ea06e4f0312c571646e5e
yuval <yuval66ts@gmail.com>
2016-05-24 12:08:21 +0300

    find RED:func

--- src/map.js
+++ src/map.js
@@ -4,15 +4,15 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-	var ret_val =[]; 
-	var i=0;
-	for(i=0;i<this._posts.length;i++){
-		var n = this._posts[i].search(name);
-		if(n!=-1){
-			ret_val.push(this._posts[i]);
+	var ret_array =[]; 
+	
+	for(var i=0;i<this._posts.length;i++){
+		
+		if(this._posts[i].search(name) !=-1){
+			ret_array.push(this._posts[i]);
 		}
 	}
-	return ret_val;
+	return ret_array;
 };
   
 Map.prototype.find_by_location = function(name) {
