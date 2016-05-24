c49fe48385a97500c51bb045aae2d2414213ed6f
maienab <maien_1991@hotmail.com>
2016-05-24 18:08:43 +0300

    GREEN:Find by location

--- src/map.js
+++ src/map.js
@@ -10,5 +10,9 @@ Map.prototype.find_a_person = function(name) {
 Map.prototype.find_a_person = function(name) {
   return ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"];
 };
-
+Map.prototype.find_by_loc = function(name) {
+  if(this.find_a_person(name).length>0)
+    return true;
+  return false;
+};
 module.exports = Map;
