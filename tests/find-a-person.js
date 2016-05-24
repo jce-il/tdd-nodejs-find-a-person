5dd8b7d19076f419508c49d475d7cf4e32990762
yuval <yuval66ts@gmail.com>
2016-05-24 12:19:49 +0300

    find inconsistencies RED:func

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -21,7 +21,7 @@ describe('Find a person by location', function() {
 describe('check for inconsistencies', function() {
   it('Check if there are map inconsistencies, e.g., the same name with different locations.', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.is_inconsistent("Or A.")
+    var posts = map.find_inconsistencies("Or A.")
     expect(posts).to.be.eql(true);
   });
 });
