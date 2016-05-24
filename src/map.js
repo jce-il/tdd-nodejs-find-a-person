6c1488cad32c8408be7beb5b4ca746750dd44501
yuval <yuval66ts@gmail.com>
2016-05-24 12:17:03 +0300

    find by loc:GREEN func

--- src/map.js
+++ src/map.js
@@ -18,6 +18,7 @@ Map.prototype.find_a_person = function(name) {
 Map.prototype.find_by_location = function(name) {
   if(this.find_a_person(name).length>0)
 		return true;
+	
 	return false;
   };
 
