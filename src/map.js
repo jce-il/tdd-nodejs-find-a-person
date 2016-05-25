bd638b2665958f746472032ecccce64b30782568
Tomer Achdut <tomera90@gmail.com>
2016-05-25 20:50:10 +0300

    GREEN: ~Fix~ A test for a location

--- src/map.js
+++ src/map.js
@@ -8,11 +8,15 @@ Map.prototype.find_a_person = function(name) {
 	for(var i =0; i<this._posts.length; i++)
 		if(this._posts[i].search(name) > -1)
 			posts.push(this._posts[i]);
-	return posts;};
+	return posts;
+};
 
-Map.prototype.find_location = function(name){
-	return true;
-}; 
+Map.prototype.find_a_location = function(name) {
+    for(var i =0; i<this._posts.length; i++)
+        if(this._posts[i].search(name) >= 0)
+            return true;
+    return false;
+};
 
 Map.prototype.find_inconsistencies = function(name){
   	return true;
