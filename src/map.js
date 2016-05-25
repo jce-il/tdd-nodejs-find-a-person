5033267f48a127abebb87e9d59ccec4dd28dbeba
Tomer Achdut <tomera90@gmail.com>
2016-05-25 20:15:44 +0300

    GREEN: ~Fix~ A test for finding a name

--- src/map.js
+++ src/map.js
@@ -1,11 +1,14 @@
 
 function Map(posts) {
-  this._posts = posts;
+	this._posts = posts;
 }
 
 Map.prototype.find_a_person = function(name) {
-  return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
-};
+	var posts = [];
+	for(var i =0; i<this._posts.length; i++)
+		if(this._posts[i].search(name) > -1)
+			posts.push(this._posts[i]);
+	return posts;};
 
 Map.prototype.find_location = function(name){
 	return true;
