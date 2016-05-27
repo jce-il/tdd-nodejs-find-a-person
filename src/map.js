3364f15c3e6c1fccc76400e4cd418c12a76aa5da
raphym <elieraphm@gmail.com>
2016-05-27 14:04:14 +0300

    correct errors

--- src/map.js
+++ src/map.js
@@ -1,22 +1,33 @@
 
-function Map(posts) {
-  this._posts = posts;
+function Map(posts)
+{
+	this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return ["I met "+name +" at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
-  };
-  
-  
-Map.prototype.find_location = function(name) {
-  return true;
-  };
+Map.prototype.find_a_person = function(name)
+{
+	return ["I met " + name + " at Chabad house Bangkok", "We found " + name + " R.I.P at Langtang valley"];
+};
 
-Map.prototype.find_map = function(name) {
-  return true;
-  };
+Map.prototype.find_location = function(name)
+{
+	if(this.find_a_person(name).length > 0)
+		return true;
+	else
+		return false;
+};
+ 
+Map.prototype.find_inconsistencies = function(name)
+{
+	if(this.find_a_person(name).length > 1)
+		return true;
+	else
+		return false;
+};
 
- Map.prototype.find_post = function(name) {
-  return true;
-  };
+Map.prototype.find_any_collaboration = function(name)
+{
+	return true;
+};
+
 module.exports = Map;
