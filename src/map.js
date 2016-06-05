8c86044c5fc55709a84917acabd9af359c053e97
effi-l <efratlorentz@gmail.com>
2016-06-05 14:05:10 +0300

    GREEN: passed test finding by location

--- src/map.js
+++ src/map.js
@@ -5,7 +5,7 @@ function Map(posts) {
 
 
 
-Map.prototype.find_a_person = function(name) {
+/*Map.prototype.find_a_person = function(name) {
 var arr=[];
 var  j=0;
 for(var i=0;i<this._posts.length;i++)
@@ -15,6 +15,19 @@ for(var i=0;i<this._posts.length;i++)
 
 
   return arr;
+
+
+  };*/
+Map.prototype.find_a_person_by_location = function(name) {
+
+for(var i=0;i<this._posts.length;i++)
+	if(this._posts[i].indexOf(name)!=-1&& (  this._posts[i].indexOf("at")||this._posts[i].indexOf("in") )  )//key words of locations
+		  return true;
+
+
+
+
+return false;
 
 
   };
