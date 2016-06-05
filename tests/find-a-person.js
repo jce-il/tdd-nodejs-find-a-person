bce4c5388530023a8ee5f77297f14d8a3ac923c1
effi-l <efratlorentz@gmail.com>
2016-06-05 14:30:36 +0300

    GREEN: fixd passed test finding by location

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,20 +9,19 @@ var Map = require('./../src/map');
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 });*/
-
-/*describe('Find a person by location', function() {
+describe('Find a person by location', function() {
   it('Given a person name, return true if there is data about the location o/w false', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post","I saw Or A. in the valley"]);
     var posts = map.find_a_person_by_location("Or A.")
     expect(posts).to.be.eql(true);
   });
-});*/
-describe('Find a person by different locations', function() {
+});
+/*describe('Find a person by different locations', function() {
   it('Given a person name, return true if there are more than one post of data of locations o/w -false', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post","I saw Or A. in the valley"]);
     var posts = map.find_a_person_by_different_locations("Or A.")
     expect(posts).to.be.eql(true);
   });
-});
+});*/
 
 
