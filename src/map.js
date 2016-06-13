fd54c69cf8e35775e512b1e0a658232bb022fdf3
uria <uriasho@gmail.com>
2016-06-13 19:07:18 +0300

    RED: Test finding a name

--- src/map.js
+++ src/map.js
@@ -3,8 +3,11 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+Map.prototype.find_a_person = function(name)
+{
+    var posts = [];
+ 	
+ 	return posts;
+};
 
 module.exports = Map;
