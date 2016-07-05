fa77bea2e0a053de9a6560c429857b5e0d99895b
LeaBloy <leabloy1@gmail.com>
2016-07-05 21:10:42 +0300

    RED: try to Find another person

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,3 +9,11 @@ describe('Find a person', function() {
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 });
+
+describe('Find another person', function() {
+  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    var map = new Map(["I met Lea at Chabad house Bangkok", "We found Lea R.I.P at Langtang valley", "Random post"]);
+    var posts = map.find_a_person("Lea")
+    expect(posts).to.be.eql(["I met Lea at Chabad house Bangkok", "We found Lea R.I.P at Langtang valley"]);
+  });
+});
