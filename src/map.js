4462adab33c111772239cac47f4c18cb64052d9f
LeaBloy <leabloy1@gmail.com>
2016-07-05 21:19:17 +0300

    Refactor:  add commit

--- src/map.js
+++ src/map.js
@@ -2,8 +2,9 @@
 function Map(posts) {
   this._posts = posts;
 }
-
+//find if a name exists on post
 Map.prototype.find_a_person = function(name) {
+	
     var result = new Array();
     this._posts.forEach(function (item) {
         if (item.indexOf(name) > 0)
