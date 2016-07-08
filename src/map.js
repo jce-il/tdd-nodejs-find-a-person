4cf4b1872b46fad080b7d86d0e4eddee9ac05311
effi-l <efratlorentz@gmail.com>
2016-07-08 13:24:44 +0300

    REFACTOR: refactor for test2 - reuse function find_a_person

--- src/map.js
+++ src/map.js
@@ -17,15 +17,14 @@ var names = ["Or A.", "Or B.", "blabla"];
     Map.prototype.check_if_person_in_location = function(name) {
  
 	var res = false;	
- 	this._posts.forEach(function(value) {
- 		if (value.indexOf(name) > -1 ) {	
+ 	var cmap = new Map(this._posts);
+ 	var person_locations = cmap.find_a_person(name);
+ 	person_locations.forEach(function(value) {	
  			locations.forEach(function(location) {
  				if (value.indexOf(location) > -1)
  					res = true;	
  			});
-			res =  res || (value.indexOf(" at ") > -1)
- 		}
-   		
+			res =  res || (value.indexOf(" at ") > -1) 		
  	}); 	
  	return res;
  
