9399e6a9bc36219aa6193ec74e1be26815e0b61f
rivkibe <rivkirom4@gmail.com>
2016-07-08 12:55:47 +0300

    green: find a person

--- src/map.js
+++ src/map.js
@@ -3,8 +3,12 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return [];
-  };
+ Map.prototype.find_a_person = function(name) {
 
+	var arr = [];
+ 	for (var i=0; i<this._posts.length; i++)
+ 		if (this._posts[i].indexOf(name) >= 0)
+ 			arr.push(this._posts[i]);
+ 	return arr;
+ };
 module.exports = Map;
