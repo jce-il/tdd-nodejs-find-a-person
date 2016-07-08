b9c0087392aa287b7907cb8699ecfc7e81bf3e9f
effi-l <efratlorentz@gmail.com>
2016-07-08 12:59:46 +0300

    GREEN: find a person with location

--- src/map.js
+++ src/map.js
@@ -14,8 +14,16 @@
     };
   
     Map.prototype.check_if_person_in_location = function(name) {
- 	
- 	return null;
+ 
+	var res = false;	
+ 	this._posts.forEach(function(value) {
+ 		if (value.indexOf(name) > -1 ) {
+ 			res =  value.indexOf(" at ") > -1;
+ 		}
+   		
+ 	}); 	
+ 	return res;
+ 
    };
  
   module.exports = Map;
