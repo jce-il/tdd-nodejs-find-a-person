e4e50ddafd4c80616c21d71e1527edc12ddd955b
effi-l <efratlorentz@gmail.com>
2016-07-08 12:53:43 +0300

    GREEN: correct code for test1

--- src/map.js
+++ src/map.js
@@ -1,10 +1,16 @@
 
-function Map(posts) {
-  this._posts = posts;
-}
+ function Map(posts) {
+   this._posts = posts;
+  }
+  
+  Map.prototype.find_a_person = function(name) {
 
-Map.prototype.find_a_person = function(name) {
-  return [];
-  };
-
-module.exports = Map;
+ 	var array = [];
+ 	this._posts.forEach(function(value){
+ 		if (value.indexOf(name) > -1)
+   			array.push(value);
+ 	}); 
+ 	return array;
+    };
+  
+  module.exports = Map;
