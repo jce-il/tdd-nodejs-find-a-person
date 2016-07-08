a41c913aa13977b95474551356dc2ade297771ab
rivkibe <rivkirom4@gmail.com>
2016-07-08 13:06:00 +0300

    green : implementing check_inconsistencies

--- src/map.js
+++ src/map.js
@@ -21,7 +21,19 @@ function Map(posts) {
  };
  
  Map.prototype.check_inconsistencies = function() {
- 	return false;
+ 	for (var i=0; i<this._posts.length; i++) {
+		
+ 		//split into words in order to find names
+ 		var post = this._posts[i].split(" ");
+ 		
+		//find person names until "at" keyword
+		for (var j=0; j<post.length && post[j] != "at"; j++) {
+ 			//if a name exists more than once return true
+ 			if (this.find_a_person(post[j]).length > 1)
+ 				return true;
+		}
+ 	}
+	return false;	
  };
  
 module.exports = Map;
