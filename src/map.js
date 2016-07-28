5134b9112d213cc77a9d5c51ddbab1baae2e9a09
Net4nel <Net4nel@gmail.com>
2016-07-28 20:45:41 +0300

    GREEN: Test for finding a inconsistencies

--- src/map.js
+++ src/map.js
@@ -7,15 +7,15 @@ function Map(posts) {
 Map.prototype.find_a_person = function(name) {
 
   var posts = [];
-      for(var i =0; i<this._posts.length; i++)
-            if(this._posts[i].search(name) >= 0)
-                posts.push(this._posts[i]);
+  for (var i=0; i<this._posts.length; i++)
+    if (this._posts[i].search(name) >= 0)
+      posts.push(this._posts[i]);
       return posts;
   };
 
 Map.prototype.find_a_location = function(name) {
-  for(var i =0; i<this._posts.length; i++)
-    if(this._posts[i].search(name) >= 0)
+  for (var i=0; i<this._posts.length; i++)
+    if (this._posts[i].search(name) >= 0)
         return true;
     return false;
 
@@ -23,7 +23,11 @@ Map.prototype.find_a_location = function(name) {
 };
 
 Map.prototype.isInconsistencies = function(name) {
-   return false;
+  var posts = this.find_a_person(name);
+
+  if (posts.length > 1)
+    return true;
+  return false;
 };
 
 module.exports = Map;
