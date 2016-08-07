456f306c53c51523c8056e4d95b6862ef4ac33a9
efratwex <efrat0527401620@gmail.com>
2016-08-07 09:56:42 +0300

    refactor : code reuse

--- src/map.js
+++ src/map.js
@@ -13,8 +13,9 @@ Map.prototype.find_a_person = function(name) {
 
 
    Map.prototype.find_a_person_with_location = function(name) {
-   for (var i=0; i<this._posts.length; i++)
- 		if (this._posts[i].indexOf(name) >= 0 && (this._posts[i].indexOf(" at ") >= 0 || this._posts[i].indexOf(" in ") >= 0))
+  var filtered_posts = this.find_a_person(name);
+ 	for (var i=0; i<filtered_posts.length; i++)
+ 		if (filtered_posts[i].indexOf(" at ") >= 0 || filtered_posts[i].indexOf(" in ") >= 0)
  			return true;
  
 	return false;
