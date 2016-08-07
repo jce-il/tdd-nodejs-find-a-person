b10b0385ecbb3d71b0c6ebda27b95df7e354002c
efratwex <efrat0527401620@gmail.com>
2016-08-07 09:31:43 +0300

    red - Test for finding a name

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -4,7 +4,7 @@ var Map = require('./../src/map');
 
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
-    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post", "Or in Jerusalem"]);
     var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
